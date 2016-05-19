cc7de93e644e52dc807c139b53900feff5149ddc
Hack Reactor Students <accounts@hackreactor.com>
2016-05-19 11:55:33 -0700

    Start transition to mongodb

--- app/models/link.js
+++ app/models/link.js
@@ -16,4 +16,16 @@ var Link = db.Model.extend({
   }
 });
 
+/*
+urlsSchema.methods.initialize = function() {
+  this.on('creating', function(model, attrs, options) {
+    var shasum = crypto.createHash('sha1');
+    shasum.update(model.get('url'));
+    model.set('code', shasum.digest('hex').slice(0, 5));
+  });
+}
+
+var Link = mongoose.model('Link', urlsSchema);
+*/
+
 module.exports = Link;
--- app/models/user.js
+++ app/models/user.js
@@ -22,4 +22,28 @@ var User = db.Model.extend({
   }
 });
 
+/*
+usersSchema.methods.initialize = function() {
+  this.on('creating', this.hashPassword);
+}
+
+usersSchema.methods.comparePassword = function(attemptedPassword, callback) {
+    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+      callback(isMatch);
+    });
+}
+
+usersSchema.methods.hashPassword = function() {
+  var cipher = Promise.promisify(bcrypt.hash);
+    return cipher(this.get('password'), null, null).bind(this)
+      .then(function(hash) {
+        this.set('password', hash);
+      });
+}
+
+var User = mongoose.model('User', usersSchema);
+
+
+*/
+
 module.exports = User;
